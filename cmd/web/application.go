f4d540fa8a70a29e4437140c82c68dbf86231231
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-03 08:46:43 +0300

    Update application.go

--- cmd/web/application.go
+++ cmd/web/application.go
@@ -84,3 +84,18 @@ func (config Application) Run() {
 		log.Fatalf("Server was unable to gracefully shutdown due to err: %+v", err)
 	}
 }
+
+func (app *Application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Println(trace)
+ 
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
